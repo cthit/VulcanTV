40502a771ccfcb8716ce8223fd1930608d22cb92
Vidar Magnusson <git@vidarmagnusson.com>
2021-01-13 17:33:48 +0100

    Add logging to videos.log

--- randomvideo.sh
+++ randomvideo.sh
@@ -4,4 +4,5 @@ git pull
 
 VIDEOID="$(grep -v '^#' videoidlist.txt | shuf -n 1 | sed 's/ .*//')";
 VIDEOURL=$(echo "https://www.youtube.com/embed/$VIDEOID?playlist=$VIDEOID&autoplay=1&mute=1&hd=0&loop=1&cc_load_policy=1&cc_lang_pref=en")
+echo $VIDEOURL >> videos.log
 ./kiosk.sh $VIDEOURL
