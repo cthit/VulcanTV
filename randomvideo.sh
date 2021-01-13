5fd5da7b9ab2d4d49d52cfdcd5fb7d06a234c536
Erik Johnsson <erik@erikjohnsson.se>
2021-01-13 16:34:44 +0000

    Add git pull and ignore

--- randomvideo.sh
+++ randomvideo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git pull
+
 VIDEOID="$(grep -v '^#' videoidlist.txt | shuf -n 1 | sed 's/ .*//')";
 VIDEOURL=$(echo "https://www.youtube.com/embed/$VIDEOID?playlist=$VIDEOID&autoplay=1&mute=1&hd=0&loop=1&cc_load_policy=1&cc_lang_pref=en")
 ./kiosk.sh $VIDEOURL
